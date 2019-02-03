0b07ed67027f0bf8659b22d4e35149757c31260e
fuxxkk <315768455@qq.com>
2019-02-03 13:28:35 +0800

    序列化

--- rnn/test/List_Test.py
+++ rnn/test/List_Test.py
@@ -1,4 +1,22 @@
+import pickle
+# save_file = open("/Applications/pywork/machhine_learning/rnn/test/list_file", "wb+")
+load_file = open("/Applications/pywork/machhine_learning/rnn/test/list_file", "rb+")
 l = list()
-
 l.append("a")
 print(l)
+
+
+# def save_list(file):
+#     pickle.dump(l, file)
+    # save_file.write(data)
+    # save_file.close()
+
+
+# save_list(save_file)
+
+def load(file):
+    ll=pickle.load(file)
+    print(ll)
+    print(type(ll))
+
+load(load_file)
